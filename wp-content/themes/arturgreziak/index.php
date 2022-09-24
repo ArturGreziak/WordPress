3fd2d1a58b1be0e134d9bd1086b2b829a5270de9
ArturGreziak <alex4448@wp.pl>
2022-09-25 00:46:41 +0200

    początek stylowania sekcji z produktami

--- wp-content/themes/arturgreziak/index.php
+++ wp-content/themes/arturgreziak/index.php
@@ -1,5 +1,38 @@
 <?php
 get_header();
 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+        echo '<h1 class="page_title_w">'.get_the_title().'</h1>';
+        echo '<p>'.get_the_content().'</p>';
+        
+        
+	} 
+}
 
+echo '<h1 class="main_section_description_w">Ostatnie wpisy</h2>';
+echo '<div class="last_post_section_w">';
+$args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+     'posts_per_page' => -1,
+      'order' => 'DESC'
+);
+
+$the_query = new WP_Query($args);
+if($the_query -> have_posts()){
+    while($the_query -> have_posts()){
+        $the_query -> the_post();
+        echo '<div class="last_post_item_w">';
+            the_title();
+            echo '</br>';
+            the_content();
+            echo '</br>'; 
+        echo '</dvi>';
+    }
+}
+
+wp_reset_postdata();
+echo '</div>';
 get_footer();
