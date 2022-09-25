f103c54aee28cba51251ad9a89dabe4a731e684f
ArturGreziak <alex4448@wp.pl>
2022-09-25 16:52:21 +0200

    poprawki w index.php i style.css

--- wp-content/themes/arturgreziak/index.php
+++ wp-content/themes/arturgreziak/index.php
@@ -5,19 +5,19 @@ if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
         echo '<h1 class="page_title_w">'.get_the_title().'</h1>';
-        echo '<p>'.get_the_content().'</p>';
+        echo '<p class="main_content_w">'.get_the_content().'</p>';
         
         
 	} 
 }
 
 echo '<h1 class="main_section_description_w">Ostatnie wpisy</h2>';
-echo '<div class="last_post_section_w">';
+echo '<div class="last_posts_section_w">';
 $args = array(
     'post_type' => 'post',
     'post_status' => 'publish',
-     'posts_per_page' => -1,
-      'order' => 'DESC'
+     'posts_per_page' => 3,
+      'order' => 'ASC'
 );
 
 $the_query = new WP_Query($args);
@@ -35,4 +35,7 @@ if($the_query -> have_posts()){
 
 wp_reset_postdata();
 echo '</div>';
+
+
+
 get_footer();
